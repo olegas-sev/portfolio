f6ba9cecd2593a75bd12b2462dafeea1095acf71
Olegas Sevcenko <olegas.sevcenko@elev.ga.ntig.se>
2021-09-28 14:56:04 +0200

    Jobbade med hero sektionen och nest/image

--- components/Hero.js
+++ components/Hero.js
@@ -1,9 +1,13 @@
+import portfolio from '../public/portfolio.jpg'
+import Image from 'next/image'
+
+console.log(portfolio);
 function Hero() {
     return (
         <section>
             <div className="main-container py-28 flex flex-row" >
-                <div className="homer-hero-wrapper flex items-center justify-between py-5">
-                    <div className="homer-hero-left mr-5">
+                <div className="homer-hero-wrapper flex flex-col items-center justify-between py-5 sm:flex-row">
+                    <div className="homer-hero-left mr-5 w-[65%]">
                     <h1 className="text-custom-gray text-5xl font-semibold mb-8">
                     Hello, I am Olegas.<br />A web developer.
                 </h1>
@@ -11,11 +15,15 @@ function Hero() {
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit dapibus porta lorem morbi hendrerit. Maecenas et, at quis purus.
                 </p>
                     </div>
-                    <div className="homer-hero-right">
-                        <img src="https://assets.website-files.com/5fd908fb4e19797598a2f9ed/5fd923e456b71f08e5899b2e_image-home-hero-portfolio-x-template.jpg" className="rounded-2xl w-full max-w-md" alt="pic" />
+                    <div className="homer-hero-right relative h-[298px] w-[35%]">
+                        <Image 
+                        src={portfolio.src} 
+                        className="rounded-2xl w-full max-w-md" 
+                        layout="fill"
+                        objectFit="cover"
+                        />
                     </div>
                 </div>
-
             </div>
         </section>
     )
