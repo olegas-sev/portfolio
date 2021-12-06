6a9da7ef4213bf64254282d25d4638037c5e03bf
olegas-sev <olegas.sevcenko@elev.ga.ntig.se>
2021-12-06 19:11:39 +0100

    test

--- pages/index.js
+++ pages/index.js
@@ -21,12 +21,12 @@ export default function Home() {
                 <meta property="og:description" content="Full Stack Developer with strong knowledge of Javascript, modern technologies & frameworks such as React, Vue & Express" />
                 <meta property="og:url" content="https://olegas.vercel.app" />
                 <meta property="og:type" content="website" />
-                <meta property="og:image" content={portfolioImg.src} />
-                <meta property="og:image:secure_url" content={portfolioImg.src} />
+                <meta property="og:image" content={portfolioImg} />
+                <meta property="og:image:secure_url" content={portfolioImg} />
                 <meta property="og:image:width" content="1280" />
                 <meta property="og:image:height" content="640" />
                 <meta property="twitter:card" content="summary_large_image" />
-                <meta property="twitter:image" content={portfolioImg.src} />
+                <meta property="twitter:image" content={portfolioImg} />
                 <meta property="twitter:site" content="@webdevhusky" />
 
                 {/* <meta name="description" content="Full Stack Developer with strong knowledge of Javascript, modern technologies & frameworks such as React, Vue & Express" />
