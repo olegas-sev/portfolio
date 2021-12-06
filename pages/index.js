f4f6a9097029d0ffebac2cfc4d769b567bc9a46d
olegas-sev <olegas.sevcenko@elev.ga.ntig.se>
2021-12-06 19:03:59 +0100

    Fixed open graph protocol

--- pages/index.js
+++ pages/index.js
@@ -17,21 +17,17 @@ export default function Home() {
                 <title>Olegas Sevcenko | Portfolio</title>
                 <link rel="icon" href="/favicon.ico" />
 
-                <meta property="og:site_name" content="Colby Fayock" />
-                <meta property="og:title" content="Anyone Can Map! Inspiration and an introduction to the world of mapping - Colby Fayock" />
-                <meta property="og:description" content="Chef Gusteau was a visionary who created food experiences for the world to enjoy. How can we take his lessons and apply them to the world of…" />
-                <meta property="og:url" content="https://www.colbyfayock.com/2020/03/anyone-can-map-inspiration-and-an-introduction-to-the-world-of-mapping/" />
-                <meta property="og:type" content="article" />
-                <meta property="article:publisher" content="https://www.colbyfayock.com" />
-                <meta property="article:section" content="Coding" />
-                <meta property="article:tag" content="Coding" />
-                <meta property="og:image" content="https://res.cloudinary.com/fay/image/upload/w_1280,h_640,c_fill,q_auto,f_auto/w_860,c_fit,co_rgb:232129,g_west,x_80,y_-60,l_text:Source%20Sans%20Pro_70_line_spacing_-10_semibold:Anyone%20Can%20Map!%20Inspiration%20and%20an%20introduction%20to%20the%20world%20of%20mapping/blog-social-card-1.1" />
-                <meta property="og:image:secure_url" content="https://res.cloudinary.com/fay/image/upload/w_1280,h_640,c_fill,q_auto,f_auto/w_860,c_fit,co_rgb:232129,g_west,x_80,y_-60,l_text:Source%20Sans%20Pro_70_line_spacing_-10_semibold:Anyone%20Can%20Map!%20Inspiration%20and%20an%20introduction%20to%20the%20world%20of%20mapping/blog-social-card-1.1" />
+                <meta property="og:title" content="Olegas Sevcenko | Portfolio" />
+                <meta property="og:description" content="Full Stack Developer with strong knowledge of Javascript, modern technologies & frameworks such as React, Vue & Express" />
+                <meta property="og:url" content="https://olegas.vercel.app" />
+                <meta property="og:type" content="website" />
+                <meta property="og:image" content={coverImg.src} />
+                <meta property="og:image:secure_url" content={coverImg.src} />
                 <meta property="og:image:width" content="1280" />
                 <meta property="og:image:height" content="640" />
                 <meta property="twitter:card" content="summary_large_image" />
-                <meta property="twitter:image" content="https://res.cloudinary.com/fay/image/upload/w_1280,h_640,c_fill,q_auto,f_auto/w_860,c_fit,co_rgb:232129,g_west,x_80,y_-60,l_text:Source%20Sans%20Pro_70_line_spacing_-10_semibold:Anyone%20Can%20Map!%20Inspiration%20and%20an%20introduction%20to%20the%20world%20of%20mapping/blog-social-card-1.1" />
-                <meta property="twitter:site" content="@colbyfayock" />
+                <meta property="twitter:image" content={coverImg.src} />
+                <meta property="twitter:site" content="@webdevhusky" />
 
                 {/* <meta name="description" content="Full Stack Developer with strong knowledge of Javascript, modern technologies & frameworks such as React, Vue & Express" />
                 <meta property="og:url" content="https://www.olegas.vercel.app" />
