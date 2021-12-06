79025878b59f70f774bad3aada81894be2e3a2fc
Olegas Sevcenko <olegas.sevcenko@elev.ga.ntig.se>
2021-12-06 17:46:27 +0100

    Open graph protocol

--- pages/index.js
+++ pages/index.js
@@ -13,6 +13,10 @@ export default function Home() {
             <Head>
                 <title>Olegas Sevcenko | Portfolio</title>
                 <link rel="icon" href="/favicon.ico" />
+                <meta property="og:title" content="Olegas Sevcenko - Full Stack Developer | Portfolio" />
+                <meta property="og:type" content="website" />
+                <meta property="og:image" content="https://olegas.vercel.app/_next/static/image/public/assets/images/portfolio.98a414c70aae1ce9c0d9163bfbf2c72c.jpg" />
+                <meta property="og:url" content="https://www.olegas.vercel.app" />
             </Head>
             <MainLayout>
                 <Hero />
